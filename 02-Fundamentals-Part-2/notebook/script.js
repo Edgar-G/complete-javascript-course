171ecbafc814ba8ae7efbb06eb32c1d3f2ecec31
Edgar Aguilar <Takkunaso@gmail.com>
2022-02-22 12:41:25 -0500

    + 3 lesson progress
    Next class = 34

---
+++ 02-Fundamentals-Part-2/notebook/script.js
@@ -0,0 +1,36 @@
+'use strict';
+// Can only be used for a specific mode and funciton. ALways put strict mode in the beginning of a script and like that write mode secure code. Strict mode allows to avoid accidental errors. strict shows visible errors in some situations.
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log(`I can drive :D`);
+
+// const interface = `Audio`;
+// const private = 345;
+
+// CH33 Functions
+
+// function logger() {
+//     console.log(`My name is Edgar`);
+// }
+
+// // running / invoking / calling the funciton
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(6, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(6, 0));
+
+// const appleOrangeJuice = fruitProcessor(4, 4);
+// console.log(appleOrangeJuice);
+
+// Keep your code DRY (Dont repeat yourself) this is why functions are super useful.
+
+// CH34 Function Declarations vs. Expressions
